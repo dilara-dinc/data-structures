7acd0df87a3f3f29829adae7b95d81d67ccd4384
dilara-dinc <dilaradinc.0009@gmail.com>
2026-04-05 10:37:37 +0300

    implement graph

---
+++ graph/graph.py
@@ -0,0 +1,12 @@
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        self.graph[u].append(v)
+
+    def print_graph(self):
+        for node in self.graph:
+            print(f"{node} -> {self.graph[node]}")
